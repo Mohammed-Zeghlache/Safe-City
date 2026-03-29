3a93397dffe5ba601228fa51f42cbc51535e88ea
Mohamed <mouhamedzeghlache7@gmail.com>
2026-03-29 19:26:36 +0200

    fix: corrected image path case sensitivity

--- src/FTC_Project/Lading_Page/Lading3/Lading3.jsx
+++ src/FTC_Project/Lading_Page/Lading3/Lading3.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "../Lading3/Lading3.css";
 
-import water from '../../images/water.jpg'
-import agression from "../../images/agression.jpg";
-import déchet from "../../images/déchet.jpg";
-import eclairage2 from "../../images/eclairage2.jpg";
-import accident from "../../images/accident.jpg";
-import danger from "../../images/danger.jpg";
-import câble from "../../images/câble.jpg";
-import route from "../../images/route.jpg";
+import Water from '../../images/Water.jpg'
+import Agression from "../../images/Agression.jpg";
+import Dechet from "../../images/Dechet.jpg";
+import Eclairage2 from "../../images/Eclairage2.jpg";
+import Accident from "../../images/Accident.jpg";
+import Danger from "../../images/Danger.jpg";
+import Cable from "../../images/Cable.jpg";
+import Route from "../../images/Route.jpg";
 
 
 
@@ -20,42 +20,42 @@ const Landing3 = () => {
 
       <div className="problems-container">
         <div className="problem-card">
-          <img src={accident} alt="Accident" className="problem-image" />
+          <img src={Accident} alt="Accident" className="problem-image" />
           <p className="problem-name">Accident</p>
         </div>
 
         <div className="problem-card">
-          <img src={eclairage2} alt="Eclairage publique" className="problem-image" />
+          <img src={Eclairage2} alt="Eclairage publique" className="problem-image" />
           <p className="problem-name">Eclairage publique</p>
         </div>
 
         <div className="problem-card">
-          <img src={danger} alt="Dangers zone" className="problem-image" />
+          <img src={Danger} alt="Dangers zone" className="problem-image" />
           <p className="problem-name">Dangers zone</p>
         </div>
 
         <div className="problem-card">
-          <img src={water} alt="Water leakage" className="problem-image" />
+          <img src={Water} alt="Water leakage" className="problem-image" />
           <p className="problem-name">Water leakage</p>
         </div>
 
          <div className="problem-card">
-          <img src={déchet} alt="déchet" className="problem-image" />
+          <img src={Dechet} alt="déchet" className="problem-image" />
           <p className="problem-name">déchet</p>
         </div>
 
         <div className="problem-card">
-          <img src={câble} alt="Cable issue" className="problem-image" />
+          <img src={Cable} alt="Cable issue" className="problem-image" />
           <p className="problem-name">Cable issue</p>
         </div>
 
         <div className="problem-card">
-          <img src={agression} alt="agression" className="problem-image" />
+          <img src={Agression} alt="agression" className="problem-image" />
           <p className="problem-name">agression</p>
         </div>
 
          <div className="problem-card">
-          <img src={route} alt="route" className="problem-image" />
+          <img src={Route} alt="route" className="problem-image" />
           <p className="problem-name">Route degradee</p>
         </div>
         
